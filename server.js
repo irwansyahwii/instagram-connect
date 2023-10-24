e22a0b19214046197055e580601c91cd92d32910
Irwansyah <irwansyah@gmail.com>
2023-10-24 14:59:20 +0700

    fix: using BASE_URL and redirect to root with acces_token

--- server.js
+++ server.js
@@ -33,7 +33,7 @@ passport.use(
     {
       clientID: process.env.FACEBOOK_CLIENT_ID,
       clientSecret: process.env.FACEBOOK_CLIENT_SECRET,
-      callbackURL: `https://cpeqah8x3a.ap-southeast-1.awsapprunner.com/auth/facebook/callback`,
+      callbackURL: `${process.env.BASE_URL}/auth/facebook/callback`//`https://cpeqah8x3a.ap-southeast-1.awsapprunner.com/auth/facebook/callback`,
     },
     // <3> Verify callback
     (accessToken, refreshToken, profile, done) => {
@@ -61,16 +61,20 @@ app.get(
   passport.authenticate('facebook', {
     // <6> Scopes
     // scope: ['instagram_manage_insights', 'instagram_basic'],    
-    scope: ['user_profile','user_media']
+    scope: ['pages_show_list', 'instagram_basic', 'instagram_content_publish']
+    // scope: ['user_profile','user_media']
   })
 );
+const irwansyah_idn_token =  "EAAMK352nowgBO3ZBRIuZCdZBvzrO6nZB52bK9ZCAHSvaaT3ZB6SG1oIpZBUaghqUjMfqkUM1HQ5BSv7aanKp5AwsiAJjaRzPcfIbpoceeR8U7Uo0prbGvEdAgnwZBZBk0GKXxh10RtJmR0dW8mpIzyD5JizpMuxQKkHQMozzZCymWhKSD0Na4XMvSBoSAMS79b0iai562ZAg8V7N9uoVPdJQWZBgzf7Hwq9ekZBYKRmQ1LnHMjpd3Kn7mEFCGbbV1C1jH";
 
 app.get('/query', async function(req, res, next) {
   const igid = req.query['igid'];
   
   let response = null;
   try {
-    response = await axios.get(`https://graph.facebook.com/v18.0/17841462406046553?fields=business_discovery.username(${igid}){followers_count,media_count}&access_token=${longLiveToken.access_token}`);
+    response = await axios.get(`https://graph.facebook.com/v18.0/me/accounts&access_token=${irwansyah_idn_token}`);
+    //response = await axios.get(`https://graph.facebook.com/v18.0/147608145104866?fields=instagram_business_account&access_token=${irwansyah_idn_token}`);
+    //https://graph.facebook.com/v18.0/17841462406046553?fields=business_discovery.username(${igid}){followers_count,media_count}&access_token=${longLiveToken.access_token}`);
 
     res.json(response.data);    
       
@@ -93,31 +97,34 @@ app.get(
     try {
       // <8> Obtaining profiles
       const accessToken = req.user.accessToken;
-      const sdkProfile = await sdk.getProfile(
-        'social-media/publishing-profiles@1.0.1'
-      );
-      const result = await sdkProfile
-        .getUseCase('GetProfilesForPublishing')
-        .perform(
-          {},
-          {
-            provider: 'instagram',
-            parameters: {
-              accessToken,
-            },
-          }
-        );
-      const profiles = result.unwrap();
 
-      res.send(
-        `
-        <h1>Authentication succeeded</h1>
-        <h2>User data</h2>
-        <pre>${JSON.stringify(req.user, undefined, 2)}</pre>
-        <h2>Instagram profiles</h2>
-        <pre>${JSON.stringify(profiles, undefined, 2)}</pre>
-        `
-      );
+      res.redirect(`${process.env.BASE_URL}?access_token=${accessToken}`);
+
+      // const sdkProfile = await sdk.getProfile(
+      //   'social-media/publishing-profiles@1.0.1'
+      // );
+      // const result = await sdkProfile
+      //   .getUseCase('GetProfilesForPublishing')
+      //   .perform(
+      //     {},
+      //     {
+      //       provider: 'instagram',
+      //       parameters: {
+      //         accessToken,
+      //       },
+      //     }
+      //   );
+      // const profiles = result.unwrap();
+
+      // res.send(
+      //   `
+      //   <h1>Authentication succeeded</h1>
+      //   <h2>User data</h2>
+      //   <pre>${JSON.stringify(req.user, undefined, 2)}</pre>
+      //   <h2>Instagram profiles</h2>
+      //   <pre>${JSON.stringify(profiles, undefined, 2)}</pre>
+      //   `
+      // );
       next();
     } catch (err) {
       next(err);
